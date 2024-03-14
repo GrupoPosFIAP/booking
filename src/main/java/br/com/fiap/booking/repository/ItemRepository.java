6fc67baee10f14b78bc712662f95de2eb2046d05
Fábio Ap. Oliveira Silva <fabio.oliveira.silva@avanade.com>
2024-03-13 22:44:37 -0300

    Search funcionando

--- src/main/java/br/com/fiap/booking/repository/ItemRepository.java
+++ src/main/java/br/com/fiap/booking/repository/ItemRepository.java
@@ -1,7 +1,10 @@
 package br.com.fiap.booking.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.fiap.booking.domain.opcionais.Item;
 
+@Repository
 public interface ItemRepository extends BaseRepository<Item> {
 
 }
--- src/main/java/br/com/fiap/booking/repository/ServicoRepository.java
+++ src/main/java/br/com/fiap/booking/repository/ServicoRepository.java
@@ -1,7 +1,10 @@
 package br.com.fiap.booking.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.fiap.booking.domain.opcionais.Servico;
 
+@Repository
 public interface ServicoRepository extends BaseRepository<Servico> {
 
 }
